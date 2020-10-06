5d707fbb17f69dd3bef9958349bcde2d6e744ce7
Hendoe <hendo1349@gmail.com>
2020-10-06 18:41:08 -0500

    deploy

--- seeds/seed.aliens.sql
+++ seeds/seed.aliens.sql
@@ -1,5 +1,5 @@
 INSERT INTO aliens (alien_name, hp, atk, biomass_cost, synapse_required, description, special_features)
 VALUES
-  ('Primarch', 5, 5, 20, 0, 'Leader alien. Is able to direct the actions of simpler aliens in the Brood.', 'Produces 5 Synapse for the Brood.'),
-  ('Worker Drone', 1, 1, 5, 1, 'Simple alien. Gathers Biomass for the growth of the Brood.', 'Able to do many tasks.'),
-  ('Warrior Drone', 2, 2, 10, 1, 'Aliens bred strictly for combat purposes.', 'Will always take damage first in battle, saving the other aliens from harm.')
+  -- ('Primarch', 5, 5, 20, 0, 'Leader alien. Is able to direct the actions of simpler aliens in the Brood.', 'Produces 5 Synapse for the Brood.'),
+  ('Worker Drone', 1, 1, 5, 1, 'Simple alien. Gathers Biomass for the growth of the Brood.', 'Able to do many tasks.')
+  -- ('Warrior Drone', 2, 2, 10, 1, 'Aliens bred strictly for combat purposes.', 'Will always take damage first in battle, saving the other aliens from harm.')
--- seeds/seed.status_report.sql
+++ seeds/seed.status_report.sql
@@ -1,4 +1,5 @@
 INSERT INTO status_report (brood_name, aliens, alien_structures, biomass, synapse)
 VALUES
   ('Marlarks', 27, 6, 80, 32),
-  ('Durlongs', 5, 2, 10, 8)
+  ('Durlongs', 5, 2, 10, 8),
+  ('Starter', 1, 2, 20, 5)
